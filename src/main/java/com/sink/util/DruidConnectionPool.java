e24b4319786d18ae90d1b6755304f82865ac7673
xry <362438997@qq.com>
2021-12-11 22:03:56 +0800

    新增 两阶段提交 MySQL Sink

---
+++ src/main/java/com/sink/util/DruidConnectionPool.java
@@ -0,0 +1,33 @@
+package com.sink.util;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DruidConnectionPool {
+    private transient static DataSource dataSource = null;
+    private transient static Properties props = new Properties();
+
+    // 静态代码块
+    static {
+        props.put("driverClassName", "com.mysql.cj.jdbc.Driver");
+        props.put("url", "jdbc:mysql://localhost:3306/test_db?serverTimezone=GMT&characterEncoding=utf8");
+        props.put("username", "root");
+        props.put("password", "123456");
+        try {
+            dataSource = DruidDataSourceFactory.createDataSource(props);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DruidConnectionPool() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
