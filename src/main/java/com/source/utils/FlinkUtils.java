0c1fb2a7c32d412be11ffb240a246ed9038ddc90
xuranyang <xuranyang@git.com>
2022-01-02 02:16:19 +0800

    add utils

--- src/main/java/com/source/utils/FlinkUtils.java
+++ src/main/java/com/source/utils/FlinkUtils.java
@@ -1,12 +1,25 @@
 package com.source.utils;
 
+import java.util.List;
+
 public class FlinkUtils {
-    public <T> String ifNullToString(T data) {
+    public static <T> String ifNullToString(T data) {
         if (data == null) {
             return "";
         } else {
             return data.toString();
         }
+    }
+
+    public static <T> T getListFisrt(List<T> data) {
+        if (data == null || data.size() == 0) {
+            return null;
+        }
+        return data.get(0);
+    }
+
+    public static <T> List<T> getListTopN(List<T> data, int topN) {
+        return data.subList(0, topN);
     }
 
 }
--- src/test/java/UtilsTestMain.java
+++ src/test/java/UtilsTestMain.java
@@ -1,17 +1,47 @@
 import com.source.utils.FlinkUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UtilsTestMain {
-    private static FlinkUtils flinkUtils = new FlinkUtils();
 
     public static void main(String[] args) {
         String s1 = "hello";
         String s2 = null;
         Integer i1 = 24;
         Integer i2 = null;
-        System.out.println(flinkUtils.ifNullToString(s1));
-        System.out.println(flinkUtils.ifNullToString(s2));
-        System.out.println(flinkUtils.ifNullToString(i1));
-        System.out.println(flinkUtils.ifNullToString(i2));
+        System.out.println(FlinkUtils.ifNullToString(s1));
+        System.out.println(FlinkUtils.ifNullToString(s2));
+        System.out.println(FlinkUtils.ifNullToString(i1));
+        System.out.println(FlinkUtils.ifNullToString(i2));
 
+    }
+
+    @Test
+    public void testListUtils(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("a");
+        stringList.add("b");
+        stringList.add("c");
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+
+        String stringListFisrt = FlinkUtils.getListFisrt(stringList);
+        System.out.println(stringListFisrt);
+
+        Integer integerListFirst = FlinkUtils.getListFisrt(integerList);
+        System.out.println(integerListFirst);
+
+        System.out.println("-----------------");
+
+        int topN = 2;
+        List<String> stringListTopN = FlinkUtils.getListTopN(stringList, topN);
+        List<Integer> integerListTopN = FlinkUtils.getListTopN(integerList, topN);
+        System.out.println(stringListTopN);
+        System.out.println(integerListTopN);
     }
 }
