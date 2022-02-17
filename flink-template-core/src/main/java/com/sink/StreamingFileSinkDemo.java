0ef243f625b1c7001d4565bc8f30d728e2819146
xuranyang <xu.ranyang@yalla.live>
2022-02-17 16:37:36 +0800

    Add StreamingFileSink 自定义分区路径

--- flink-template-core/src/main/java/com/sink/StreamingFileSinkDemo.java
+++ flink-template-core/src/main/java/com/sink/StreamingFileSinkDemo.java
@@ -2,10 +2,13 @@ package com.sink;
 
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.core.fs.Path;
+import org.apache.flink.core.io.SimpleVersionedSerializer;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.BucketAssigner;
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
 import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.DateTimeBucketAssigner;
+import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.SimpleVersionedStringSerializer;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 
 import java.time.ZoneId;
@@ -20,13 +23,27 @@ public class StreamingFileSinkDemo {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        DataStreamSource<String> dataStreamSource = env.fromElements("hello", "world", "flink");
+        DataStreamSource<String> dataStreamSource = env.fromElements("hello,2022-01-01", "world,2022-01-02", "flink,2022-01-01");
         String path = "hdfs://master01:8020/tmp/";
 
         DateTimeBucketAssigner<String> bucketAssigner = new DateTimeBucketAssigner<>(
 //                "yyyy-MM-dd--HH-mm",
                 "yyyy-MM-dd",
                 ZoneId.of("Asia/Shanghai"));
+
+        // 自定义分区路径
+        BucketAssigner<String, String> udfDtBucketAssigner = new BucketAssigner<String, String>() {
+            @Override
+            public String getBucketId(String element, Context context) {
+                String dt = element.split(",")[1];
+                return dt;
+            }
+
+            @Override
+            public SimpleVersionedSerializer<String> getSerializer() {
+                return SimpleVersionedStringSerializer.INSTANCE;
+            }
+        };
 
         StreamingFileSink<String> streamingFileSink = StreamingFileSink
                 .forRowFormat(new Path(path), new SimpleStringEncoder<String>("UTF-8"))
@@ -39,7 +56,8 @@ public class StreamingFileSinkDemo {
                                 // 设置每个文件的最大大小 ,默认是128M
                                 .withMaxPartSize(1024 * 1024 * 1024)
                                 .build())
-                .withBucketAssigner(bucketAssigner)
+//                .withBucketAssigner(bucketAssigner)
+                .withBucketAssigner(udfDtBucketAssigner)
                 .build();
 
         dataStreamSource.addSink(streamingFileSink);
